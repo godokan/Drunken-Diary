65be0bd82a5f9d475744cb8cd0e07083378c1298
godokan <nonamekoo@naver.com>
2022-11-21 16:35:00 +0900

    리스트뷰 -> 리사이클뷰

--- app/src/main/java/com/example/drunkendiary/DetailAccess.java
+++ app/src/main/java/com/example/drunkendiary/DetailAccess.java
@@ -14,24 +14,25 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class DetailAccess extends Activity {
 
     EditText editName, editMemo;
     View dialogView;
     String sql = "";
+    Cursor resultSet;
+    ArrayAdapter<String> adapter;
+    ListView listView;
+    DateManager dateManager = new DateManager();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detailaccess);
 
-        ListView listView = (ListView) findViewById(R.id.listView);
+        listView = (ListView) findViewById(R.id.listView);
         Button btnAdd = (Button) findViewById(R.id.btnAdd);
-
-        DateManager dateManager = new DateManager();
-        editName = (EditText) findViewById(R.id.editName);
-        editMemo = (EditText) findViewById(R.id.editMemo);
 
         Intent intent = getIntent();
         final String today = intent.getStringExtra("Date");
@@ -41,20 +42,19 @@ public class DetailAccess extends Activity {
 
         try {
             sql = "select "+TableInfo.COLUMN_NAME_DTYPE+","+TableInfo.COLUMN_NAME_DNAME+","+TableInfo.COLUMN_NAME_DATE+","+TableInfo.COLUMN_NAME_TIME+" from " + TableInfo.TABLE_NAME + " where " + TableInfo.COLUMN_NAME_DATE + " = ?";
-            Cursor resultSet = db.rawQuery(sql, new String[]{today});
-            String[] res = new String[resultSet.getCount()];
-            while (resultSet.moveToNext()) {
-                String type = resultSet.getString(0);
-                String name = resultSet.getString(1);
-                String date = resultSet.getString(2);
-                String time = resultSet.getString(3);
-
-                res[resultSet.getPosition()] = type + " " + name + " " + date + " - " + time;
-            }
-            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, res);
-            listView.setAdapter(adapter);
-            resultSet.close();
+            resultSet = db.rawQuery(sql, new String[]{today});
         } catch (Exception e) {e.printStackTrace();}
+        String[] res = new String[resultSet.getCount()];
+        while (resultSet.moveToNext()) {
+            String type = resultSet.getString(0);
+            String name = resultSet.getString(1);
+            String date = resultSet.getString(2);
+            String time = resultSet.getString(3);
+
+            res[resultSet.getPosition()] = type + " " + name + " " + date + " - " + time;
+        }
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, res);
+        listView.setAdapter(adapter);
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -76,7 +76,9 @@ public class DetailAccess extends Activity {
                                     ") values  (?,?,?,?,?)";
                             db.execSQL(sql, new String[]{checked.getText().toString(), editName.getText().toString(), editMemo.getText().toString(), dateManager.getNowDate(), dateManager.getNowTime()});
                             System.out.println("입력 성공");
-                        } catch (Exception e) {e.printStackTrace();}
+                        } catch (Exception e) {e.printStackTrace();} finally {
+                            Toast.makeText(getApplicationContext(),"기록 추가 완료", Toast.LENGTH_LONG).show();
+                        }
                     }
                 });
                 dlg.setNegativeButton("취소", null);
