059ac19905f14141f09ff3dbaaba68595f13f6cb
godokan <nonamekoo@naver.com>
2022-11-21 11:47:23 +0900

    하드코딩 수정, 다이얼로그 코드 초기작성

--- app/src/main/java/com/example/drunkendiary/DetailAccess.java
+++ app/src/main/java/com/example/drunkendiary/DetailAccess.java
@@ -1,6 +1,7 @@
 package com.example.drunkendiary;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -8,26 +9,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.RadioGroup;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.Locale;
-
 public class DetailAccess extends Activity {
     RadioGroup drinkType;
-
+    EditText editName, editMemo;
+    View dialogView;
+    String sql = "";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.detailaccess);
-        DateManager dateManager = new DateManager();
 
         ListView listView = (ListView) findViewById(R.id.listView);
         Button btnAdd = (Button) findViewById(R.id.btnAdd);
+
+        editName = (EditText) findViewById(R.id.editName);
+        editMemo = (EditText) findViewById(R.id.editMemo);
 
         Intent intent = getIntent();
         final String today = intent.getStringExtra("Date");
@@ -36,7 +37,7 @@ public class DetailAccess extends Activity {
         SQLiteDatabase db = helper.getWritableDatabase();
 
         try {
-            String sql = "select dtype,dname,date,time from " + TableInfo.TABLE_NAME + " where " + TableInfo.COLUMN_NAME_DATE + " = ?";
+            sql = "select "+TableInfo.COLUMN_NAME_DTYPE+","+TableInfo.COLUMN_NAME_DNAME+","+TableInfo.COLUMN_NAME_DATE+","+TableInfo.COLUMN_NAME_TIME+" from " + TableInfo.TABLE_NAME + " where " + TableInfo.COLUMN_NAME_DATE + " = ?";
             Cursor resultSet = db.rawQuery(sql, new String[]{today});
             String[] res = new String[resultSet.getCount()];
             while (resultSet.moveToNext()) {
@@ -47,14 +48,21 @@ public class DetailAccess extends Activity {
 
                 res[resultSet.getPosition()] = type + " " + name + " " + date + " - " + time;
             }
-            ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, res);
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, res);
             listView.setAdapter(adapter);
-        } catch (Exception e) {System.out.println(e);}
+            resultSet.close();
+        } catch (Exception e) {e.printStackTrace();}
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                dialogView = (View) View.inflate(DetailAccess.this, R.layout.dialoginit,null);
+                AlertDialog.Builder dlg = new AlertDialog.Builder(DetailAccess.this);
+                dlg.setTitle("기록 추가");
+                dlg.setView(dialogView);
+                dlg.setPositiveButton("확인", null);
+                dlg.setNegativeButton("취소", null);
+                dlg.show();
             }
         });
     }
